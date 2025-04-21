67b325c3b41e45ab0b9185b4bdbaf37ba686ecd0
zCarlosAM <caaguirre@uamv.edu.ni>
2025-04-21 11:45:50 -0600

    Ahora se valida que el rango inicial sea menor que el rango final

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,12 @@
 range_min = int(input("Ingrese el inicio de un rango: "))
 range_max = int(input("Ingrese el limite de ese rango: "))
-num = int(input("Ingrese un número que se encuentre entre ese rango: "))
 
-if(num >= range_min and num <= range_max):
-    print(f"El número {num} está dentro del rango.")
+if(range_min < range_max):
+    num = int(input("Ingrese un número que se encuentre entre ese rango: "))
+    if(num >= range_min and num <= range_max):
+        print(f"El número {num} está dentro del rango.")
+    else:
+        print(f"El número {num} está fuera del rango.")
+
 else:
-    print(f"El número {num} está fuera del rango.")
+    print("Error: El inicio del rango no puede ser mayor al límite del rango.")
